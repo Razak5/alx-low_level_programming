525f45bab7fd9b4d2f0f593f32c896a004a90b40
Razak5 <hrazak50@gmail.com>
2023-02-17 13:48:05 -0800

    to show size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+int main(){
+
+	int i;
+	double d;
+	char c;
+
+	printf("int size: %lu.\n", sizeof(i));
+	printf("double size: %lu.\n", sizeof(d));
+	printf("char size: %lu. \n", sizeof(c));
+	
+	return(0);
+}
