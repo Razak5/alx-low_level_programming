e3edddd6edda6dbb88c4581d6c9c163d77749505
Razak5 <hrazak50@gmail.com>
2023-02-17 22:45:36 -0800

    program compiled

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,15 @@ int main(void)
 int i;
 double d;
 char c;
-printf("int size: %lu.\n", sizeof(i));
-printf("double size: %lu.\n", sizeof(d));
-printf("char size: %lu. \n", sizeof(c));
+long int li;
+long long ll;
+float f;
+
+printf("size of a char: %lu.\n", sizeof(c));
+printf("size of an int: %lu.\n", sizeof(i));
+printf("sinze of a long int: %lu. \n", sizeof(li));
+printf("sinze of a long long: %lu. \n", sizeof(ll));
+printf("sinze of a float: %lu. \n", sizeof(f));
 
 return (0);
 }
