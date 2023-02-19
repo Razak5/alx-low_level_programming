f6314ba5a15c344c27f85a391c9839da410fd2f9
Razak5 <hrazak50@gmail.com>
2023-02-19 05:57:26 -0800

    betty at it again -solve

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - printing to stderr
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-fprintf(stderr,"%s","\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 return(1);
 }
