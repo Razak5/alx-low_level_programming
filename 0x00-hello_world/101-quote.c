9893fc5443505a3179535e8c46830ce5b1139fe5
Razak5 <hrazak50@gmail.com>
2023-02-19 06:12:16 -0800

    again betty - code continued on next line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 20
-		15-10-19\n");
+fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 20\
+15-10-19\n");
 
 return(1);
 }
