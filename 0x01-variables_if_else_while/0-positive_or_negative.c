8f7edec18654a6af1ebefc5587930298e86bb0c9
Razak5 <hrazak50@gmail.com>
2023-02-19 18:05:43 -0800

    added new if, elseif, else test

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdion.h>
+
+/**
+ * main - prints if number stored is positve, zero or negative
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if(n > 0){
+	printf("%d is positive\n");
+	}
+	else if(n ==0){
+	printf("%d is zero\n");
+	}
+	else printf("%d is negative\n");
+	
+	return (0);
+}
