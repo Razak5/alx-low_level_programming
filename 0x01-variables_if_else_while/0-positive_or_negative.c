9c38cc42e5bc0b008655232cb1c9170f71653a85
Razak5 <hrazak50@gmail.com>
2023-02-19 18:28:26 -0800

    corrected white spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,8 @@
 
 /**
  * main - prints if number stored is positve, zero or negative
- * Return - Always (Success)
+ * Return:  Always (Success)
+ *
  */
 
 int main(void)
@@ -14,11 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n > 0)
+	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-	else if(n == 0) 
+	else if (n == 0) 
 	{
 	printf("%d is zero\n", n);
 	}
