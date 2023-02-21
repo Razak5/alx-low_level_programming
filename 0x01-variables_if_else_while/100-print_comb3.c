b756cc1528c6bfead8567cfa2b61fabe83472a8a
Razak5 <hrazak50@gmail.com>
2023-02-20 20:47:21 -0800

    all combos of 2 digit umbers can now be printed

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- prints all possible combinations of 2-digit numbers.
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num <= 99; num++)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+if (num != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
