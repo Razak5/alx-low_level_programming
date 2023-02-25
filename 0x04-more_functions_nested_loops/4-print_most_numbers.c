6b234ae9ff797e74064572656386dd21c6c77241
Razak5 <hrazak50@gmail.com>
2023-02-24 17:51:36 -0800

    printed all numbers from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - check for uppercase letters.
+ * print_most_numbers - prints from 0 to 9 except  2 and 4
  * Return: nothing -void
  */
 void print_most_numbers(void)
