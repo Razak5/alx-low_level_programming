16a36af9de078750762c40dff1a217d3a0611548
Razak5 <hrazak50@gmail.com>
2023-02-24 16:10:32 -0800

    just checked for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isdigit - check the code
+ *@c: the digit we are checking
+ * Return: if digit, 1. otherwise, 0
+ */
+int _isdigit(int c)
+{
+	if (_isdigit(c))
+	return (1);
+
+else
+	return (0);
+}
