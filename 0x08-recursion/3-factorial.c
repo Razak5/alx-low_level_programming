f99617cada372f87979ecff836c6fbcffc59b516
Razak5 <hrazak50@gmail.com>
2023-03-08 17:31:29 -0800

    factorial of a given number found

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+/**
+ * factorial - returns the factorial of given number
+ * @n: number whose factorial we need ;D
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	
+		
+	if (n == 0)
+{
+		return (1);
+}
+	return (n * factorial(n - 1));
+}
