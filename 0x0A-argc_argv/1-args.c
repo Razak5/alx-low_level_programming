73f813f6dc5f894da44d408ccb2e631f26a760f3
Razak5 <hrazak50@gmail.com>
2023-03-12 11:02:45 -0700

    number of args printed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main -print the program name
+ * @argc: argument count
+ * @argv: array of pointer to char
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+
+{
+		printf("%d\n", argc);
+	}
+
+return (0);
+}
