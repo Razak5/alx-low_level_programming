cec5a653e93815c1c3be1725c7a71413389c7a16
Razak5 <hrazak50@gmail.com>
2023-03-13 02:12:23 -0700

    argsc now starts from 0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main -print number of args passed to program
  * @argc: argument count
@@ -10,6 +9,7 @@
 int main(int argc, char *argv[])
 
 {
+	(void) argv;
 		printf("%d\n", argc - 1);
 
 return (0);
