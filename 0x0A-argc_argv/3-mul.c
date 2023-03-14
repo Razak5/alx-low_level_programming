41f07e51bbcc9c0d8faa511f9daf05745ed3b1db
Razak5 <hrazak50@gmail.com>
2023-03-14 13:04:27 -0700

    added 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main -prints the product of argv[1] and argv[2]
+ * _atoi - converts a string to an integer
+ * @s: the  string to be converted
+ * Return: the converted string-int
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+int d = 0;
+int n = 0;
+int len = 0;
+int f = 0;
+int digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+
+return (n);
+}
+/**
+ * main -prints the product of argv[2] and argv[3]
  * @argc: argument count
  * @argv: array of pointer to char
  * Return: zero
@@ -12,13 +51,8 @@
 int main(int argc, char *argv[])
 {
 
-	int i;
 
-	int mult;
-
-	int num1;
-
-	int num2;
+	int mult, num1, num2;
 
 if (argc < 3 || argc > 3)
 
@@ -30,9 +64,9 @@ if (argc < 3 || argc > 3)
 
 }
 
-	num1 = atoi(argv[1]);
+	num1 = _atoi(argv[1]);
 
-	num2 = atoi(argv[2]);
+	num2 = _atoi(argv[2]);
 
 
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,4 +3,5 @@
 		
 int _putchar(char c);
 int main(int argc, char *argv[]);
+int_atoi(char *s);
 #endif
